b779cd33dafe063710ed0ee6aab85800392afe6e
vinodbistvb <vinodbistvb@gmail.com>
2021-09-12 22:42:19 +0530

    Header adn Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Dishdetails from './DishdetailComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId });
+    }
 
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <Dishdetails dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+                <Dishdetails dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Main;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 const RenderMenuItems = ({ dish, onClick }) => {
+    return(
     <Card onClick={() => onClick(dish.id)}>
         <CardImg width="100%" src={dish.image} alt={dish.name} />
         <CardImgOverlay right className="ml-5">
             <CardTitle>{dish.name}</CardTitle>
         </CardImgOverlay>
-    </Card>
+    </Card>);
 }
 
 const Menu = (props) => {
